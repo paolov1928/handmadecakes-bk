c5685381f3d4914610871ae6fc874844034b798d
paolov1928 <ventura.paolo@gmail.com>
2019-05-30 15:57:56 +0100

    commit of the cake back end

---
+++ server.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+
+const express = require("express"),
+  app = express(),
+  port = process.env.PORT,
+  mongoose = require("mongoose"),
+  Cake = require("./api/models/cakeListModel"), //created model loading here
+  bodyParser = require("body-parser")
+  const cors = require("cors")
+
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise
+mongoose.connect("mongodb://localhost/Cakedb")
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(cors())
+
+var routes = require("./api/routes/cakeListRoutes") //importing route
+routes(app) //register the route
+
+app.listen(port)
+
+console.log("cake list RESTful API server started on: " + port)
+
+// Example post request
+// fetch("http://localhost:1337/cakes", {
+//   method: "post",
+//   body: JSON.stringify({ name: "Paolo" }),
+//   headers: {
+//     Accept: "application/json",
+//     "Content-Type": "application/json"
+//   }
+// })
+//   .then(j => j.json())
+//   .then(d => console.log(d))
